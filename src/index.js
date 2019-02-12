f95c557ae7ccb5360a4cb2e37cc64eeeb1ef843b
Michael <michael.loukeris@icloud.com>
2019-02-12 11:25:18 +0200

    adding todo

--- src/index.js
+++ src/index.js
@@ -10,3 +10,23 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+// 1. Create functional component person
+// 2. Passing props and children to component
+// 3. Working with component's state (setState)
+// 4. Pass data to onclick function
+// 5. Two way data binding
+// 6. Styling with .css file and inline
+
+// 7. Load content conditionally
+// 8. Loading a list
+// 9. Remove an item of the list
+// 10. Two-way data binding for each element
+
+// 11. Change inline styles based on condition
+// 12. Set css classses dynamically using an array of styles
+// 13. Install Radium and use it with a pseudo function
+// 14. Use media queries with radium
+// 15. Converting css files to modules
